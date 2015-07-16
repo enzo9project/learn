0c63b97005d7e4b5383c2e02e1ac2ebc315aabbf
Gongzhi Kan <kangongzhi@gmail.com>
2015-07-16 22:35:19 +1000

    match find - swipe card initial setup done

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -5,39 +5,52 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'ionic.contrib.ui.cards', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                //cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
-})
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleLightContent();
+            }
+        });
+    })
 
-.config(function($stateProvider, $urlRouterProvider) {
+    .directive('noScroll', function ($document) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+        return {
+            restrict: 'A',
+            link: function ($scope, $element, $attr) {
 
-  // setup an abstract state for the tabs directive
-      .state('home', {
-        url: '/home',
-        templateUrl: 'templates/home.html'
-      });
+                $document.on('touchmove', function (e) {
+                    e.preventDefault();
+                });
+            }
+        }
+    })
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/home');
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-});
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
+
+            // setup an abstract state for the tabs directive
+            .state('home', {
+                url: '/home',
+                templateUrl: 'templates/home.html'
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/home');
+
+    });
--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,4 +1,66 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope) {
-});
+    .controller('HomeCtrl', function ($scope) {
+    })
+
+    .controller('CardsCtrl', function ($scope, $ionicSwipeCardDelegate) {
+        //var cardTypes = [{
+        //    title: 'Swipe down to clear the card',
+        //    image: 'img/pic.png'
+        //}, {
+        //    title: 'Where is this?',
+        //    image: 'img/pic.png'
+        //}, {
+        //    title: 'What kind of grass is this?',
+        //    image: 'img/pic2.png'
+        //}, {
+        //    title: 'What beach is this?',
+        //    image: 'img/pic3.png'
+        //}, {
+        //    title: 'What kind of clouds are these?',
+        //    image: 'img/pic4.png'
+        //}];
+
+        var cardTypes = [{
+            title: 'Swipe down to clear the card',
+            image: 'img/1.jpg'
+        }, {
+            title: 'Where is this?',
+            image: 'img/2.jpeg'
+        }, {
+            title: 'What kind of grass is this?',
+            image: 'img/3.jpg'
+        }, {
+            title: 'What beach is this?',
+            image: 'img/4.jpg'
+        }, {
+            title: 'What kind of clouds are these?',
+            image: 'img/5.jpg'
+        }];
+
+        $scope.cards = Array.prototype.slice.call(cardTypes, 0, 0);
+
+        $scope.cardSwiped = function (index) {
+            $scope.addCard();
+        };
+
+        $scope.cardDestroyed = function (index) {
+            $scope.cards.splice(index, 1);
+        };
+
+        $scope.addCard = function () {
+            var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+            newCard.id = Math.random();
+            $scope.cards.push(angular.extend({}, newCard));
+        }
+
+        $scope.cardSwiped();
+    })
+
+    .controller('CardCtrl', function ($scope, $ionicSwipeCardDelegate) {
+        $scope.goAway = function () {
+            var card = $ionicSwipeCardDelegate.getSwipeableCard($scope);
+            card.swipe();
+        };
+    })
+;
